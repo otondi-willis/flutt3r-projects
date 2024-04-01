296698c012f4922e2b50c185f4a1e7e801eb5566
otondi-willis <willis.otondi.ke@gmail.com>
2024-04-01 19:58:25 +0300

    injected state

--- statemanagement/lib/main.dart
+++ statemanagement/lib/main.dart
@@ -4,23 +4,53 @@ void main() {
   runApp(const App());
 }
 
+const List<String> urls = [
+  "https://live.staticflickr.com/7372/12502775644_acfd415fa7_w.jpg",
+  "https://live.staticflickr.com/7372/12502775644_acfd415fa7_w.jpg",
+  "https://live.staticflickr.com/7372/12502775644_acfd415fa7_w.jpg",
+  "https://live.staticflickr.com/7372/12502775644_acfd415fa7_w.jpg"
+];
+
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(title: 'Photo Viewer', home: Photo());
+    return const MaterialApp(
+        title: 'Photo Viewer',
+        home: GaleryPage(title: 'Image Gallery', urls: urls));
+  }
+}
+
+class GaleryPage extends StatelessWidget {
+  final String title;
+  final List<String> urls;
+
+  const GaleryPage({super.key, required this.title, required this.urls});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(title)),
+      body: GridView.count(
+        primary: false,
+        crossAxisCount: 2,
+        children: List.of(urls.map((url) => Photo(url: url))),
+      ),
+    );
   }
 }
 
 class Photo extends StatelessWidget {
-  const Photo({super.key});
+  const Photo({super.key, required this.url});
 
-  final url =
-      'https://live.staticflickr.com/65535/50489498856_67fbe52703_b.jpg';
+  final url;
 
   @override
   Widget build(BuildContext context) {
-    return Image.network(url);
+    return Container(
+      padding: const EdgeInsets.only(top: 10),
+      child: Image.network(url),
+    );
   }
 }
